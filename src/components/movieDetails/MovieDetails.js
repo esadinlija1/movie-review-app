6d980638d0f83989e0f5f845d990cab41f88dc1d
esadinlija1 <esadinlija1@etf.unsa.ba>
2024-12-01 22:04:09 +0100

    fix

--- src/components/movieDetails/MovieDetails.js
+++ src/components/movieDetails/MovieDetails.js
@@ -4,5 +4,9 @@ import { useParams } from "react-router";
 export default function MovieDetails() {
   const { id } = useParams();
   // useEffect koji poziva fetch request sa idom i lista informacije
-  return <div className="movie-details">This is details page {id}</div>;
+  return (
+    <div className="movie-details">
+      This is details page for a movie with the id:{id}
+    </div>
+  );
 }
